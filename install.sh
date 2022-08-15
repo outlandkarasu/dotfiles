c4ec7136b68999c381b875b9a2d6bfed83236b75
outlandkarasu <outland.karasu@gmail.com>
2022-08-16 08:20:07 +0900

    issue #3 rename files

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 cd `dirname $0`
 CUR_DIR=`pwd`
-ln -s ${CUR_DIR}/.vimrc ~/.vimrc
-ln -s ${CUR_DIR}/.vim ~/.vim
-ln -s ${CUR_DIR}/.zshrc ~/.zshrc
+ln -s ${CUR_DIR}/vimrc ~/.vimrc
+ln -s ${CUR_DIR}/vim ~/.vim
+ln -s ${CUR_DIR}/zshrc ~/.zshrc
 
